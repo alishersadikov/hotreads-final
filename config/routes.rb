f420330916dc3f7b3627ddef25255510ba9a1b1b
Alisher Sadikov <alishersadikov@gmail.com>
2017-02-27 16:33:57 -0700

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :reads, only: [:create]
+      resources :reads, only: [:index, :create]
     end
   end
 end
